69de9475cf345c0bb69be401f6bb4daae776b888
patrickSevans123 <patricksamuel2107@gmail.com>
2024-05-24 17:42:02 +0700

    Add text in HomePage and Fix Navbar

--- src/modules/HomeModule/index.tsx
+++ src/modules/HomeModule/index.tsx
@@ -2,7 +2,34 @@
 import React from 'react'
 
 const HomeModule = () => {
-  return <main></main>
+  return (
+    <main style={styles.main}>
+      <div>
+        <p style={styles.mediumText}>Welcome to</p>
+        <h1 style={styles.bigText}>HeyMart A13</h1>
+      </div>
+    </main>
+  )
+}
+
+const styles = {
+  main: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    textAlign: 'center',
+  },
+  mediumText: {
+    fontSize: '1.25em',
+  },
+  bigText: {
+    fontSize: '2.5em',
+    fontWeight: 'bold',
+  },
 }
 
 export default HomeModule
