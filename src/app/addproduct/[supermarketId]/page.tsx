c9c292c67be55ec92f6f11a26cd0fc2df1f921ec
patrickSevans123 <patricksamuel2107@gmail.com>
2024-05-26 23:54:20 +0700

    Done product

---
+++ src/app/addproduct/[supermarketId]/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import AddProductModule from '@/modules/AddProductModule'
+import React from 'react'
+
+const AddProductPage = () => {
+  return <AddProductModule />
+}
+
+export default AddProductPage
---
+++ src/app/manage/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+import ManageModule from '@/modules/ManageModule'
+import React from 'react'
+
+const ManagePage = () => {
+  return <ManageModule />
+}
+
+export default ManagePage
